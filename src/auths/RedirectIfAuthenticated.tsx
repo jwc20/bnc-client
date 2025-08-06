a64027c7785483e081d72fc7abf31f93d88e88bb
jwc20 <choi.jae100@gmail.com>
2025-08-06 23:40:08 +0900

    move

--- src/auths/RedirectIfAuthenticated.tsx
+++ src/auths/RedirectIfAuthenticated.tsx
@@ -1,5 +1,5 @@
 import { Navigate } from 'react-router';
-import { useUserStore } from '../../stores/userStore';
+import { useUserStore } from '../stores/userStore.ts';
 
 export function RedirectIfAuthenticated({ children }: { children: React.ReactNode }) {
     const isAuthenticated = useUserStore((state) => state.isAuthenticated());
--- src/auths/RequireAuth.tsx
+++ src/auths/RequireAuth.tsx
@@ -1,5 +1,5 @@
 import { Navigate } from 'react-router';
-import { useUserStore } from '../../stores/userStore';
+import { useUserStore } from '../stores/userStore';
 
 export function RequireAuth({ children }: { children: JSX.Element }) {
     const isAuthenticated = useUserStore((state) => state.isAuthenticated());
