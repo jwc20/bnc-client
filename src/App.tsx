f16447e5ee2a72fe5583f8115ec187b7956c3db6
jwc20 <choi.jae100@gmail.com>
2025-08-07 14:01:07 +0900

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,51 +1,101 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router';
-import AuthLayout from './layouts/AuthLayout';
-import LoginPage from './pages/LoginPage';
-import RegisterPage from './pages/RegisterPage';
-import LobbyPage from './pages/LobbyPage';
-import { RequireAuth } from './auths/RequireAuth';
-import { RedirectIfAuthenticated } from './auths/RedirectIfAuthenticated';
+import {Routes, Route} from "react-router";
+import {AuthProvider} from "./auths/AuthProvider";
+import {RequireAuth} from "./auths/RequireAuth";
+import {Layout} from "./layouts/Layout.jsx";
+import {LoginPage} from "./pages/LoginPage";
+import {LobbyPage} from "./pages/LobbyPage";
+import {RegisterPage} from "./pages/RegisterPage";
+// import { RoomPage } from "./pages/RoomPage";
+// import { UserPage } from "./pages/UserPage";
+// import GridCanvas from "./components/Canvas/GridCanvas.jsx";
+
+import {client} from "./api/client.gen";
+import "./App.css";
+
+client.setConfig({
+    baseUrl: import.meta.env.VITE_API_URL || "http://localhost:8000/",
+    headers: {
+        "Content-Type": "application/json",
+    },
+    throwOnError: false,
+});
+
+if (import.meta.env.DEV) {
+    client.interceptors.request.use((request, options) => {
+        console.log("API Request:", request.method, request.url);
+        return request;
+    });
+}
 
 export default function App() {
     return (
-        <Router>
+        <AuthProvider>
             <Routes>
-                <Route element={<AuthLayout />}>
+                <Route element={<Layout/>}>
+                    <Route path="/login" element={<LoginPage/>}/>
+                    <Route path="/register" element={<RegisterPage/>}/>
                     <Route
                         path="/"
                         element={
                             <RequireAuth>
-                                <LobbyPage />
+                                <LobbyPage/>
                             </RequireAuth>
-                        }
-                    />
-                    <Route
-                        path="/login"
-                        element={
-                            <RedirectIfAuthenticated>
-                                <LoginPage />
-                            </RedirectIfAuthenticated>
-                        }
-                    />
-                    <Route
-                        path="/register"
-                        element={
-                            <RedirectIfAuthenticated>
-                                <RegisterPage />
-                            </RedirectIfAuthenticated>
                         }
                     />
                     <Route
                         path="/lobby"
                         element={
                             <RequireAuth>
-                                <LobbyPage />
+                                <LobbyPage/>
                             </RequireAuth>
                         }
                     />
-                    {/* Add other protected pages here */}
+                    {/*<Route*/}
+                    {/*    path="/room/:roomId"*/}
+                    {/*    element={*/}
+                    {/*        <RequireAuth>*/}
+                    {/*            <RoomPage />*/}
+                    {/*        </RequireAuth>*/}
+                    {/*    }*/}
+                    {/*/>*/}
+                    {/*<Route*/}
+                    {/*    path="/user"*/}
+                    {/*    element={*/}
+                    {/*        <RequireAuth>*/}
+                    {/*            <UserPage />*/}
+                    {/*        </RequireAuth>*/}
+                    {/*    }*/}
+                    {/*/>*/}
+                    {/*<Route*/}
+                    {/*    path="/canvas"*/}
+                    {/*    element={*/}
+                    {/*        <RequireAuth>*/}
+                    {/*            <GridCanvas />*/}
+                    {/*        </RequireAuth>*/}
+                    {/*    }*/}
+                    {/*/>*/}
+
+                    <Route
+                        path="/unauthorized"
+                        element={
+                            <div className="unauthorized-page">
+                                <h1>Unauthorized</h1>
+                                <p>You don't have permission to access this page.</p>
+                                <a href="/lobby">Go back to lobby</a>
+                            </div>
+                        }
+                    />
+                    <Route
+                        path="*"
+                        element={
+                            <div className="not-found-page">
+                                <h1>404 - Page Not Found</h1>
+                                <a href="/">Go to home</a>
+                            </div>
+                        }
+                    />
                 </Route>
             </Routes>
-        </Router>
+        </AuthProvider>
     );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,28 +1,12 @@
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
-import { client } from './apis/client.gen';
-
-const queryClient = new QueryClient({
-    defaultOptions: {
-        queries: {
-            staleTime: 60000,
-        },
-    },
-});
-
-client.setConfig({
-    baseUrl: 'http://127.0.0.1:8000/',
-    // headers: {
-    //     Authorization: 'Bearer <token_from_service_client>',
-    // },
-});
+import { BrowserRouter } from 'react-router';
+import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
-        <QueryClientProvider client={queryClient}>
+        <BrowserRouter>
             <App />
-        </QueryClientProvider>
+        </BrowserRouter>
     </React.StrictMode>
 );
