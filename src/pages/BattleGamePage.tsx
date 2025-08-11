17a366afdf13d7fd09b33a0ff07ae7b0d70891cf
jw <choi.jae100@gmail.com>
2025-08-11 16:53:39 +0900

    add other player feedback pegs component

--- src/pages/BattleGamePage.tsx
+++ src/pages/BattleGamePage.tsx
@@ -166,6 +166,10 @@ export const BattleGamePage = ({ roomId }: { roomId: number }) => {
                         gameState={gameState}
                         currentPlayerToken={currentPlayerToken}
                     />
+                    <OtherPlayersFeedback
+                        gameState={gameState}
+                        currentPlayerToken={currentPlayerToken}
+                    />
                 </div>
                 {!gameEnded && !currentPlayerReachedMax ? (
                     <div className="input-section">
